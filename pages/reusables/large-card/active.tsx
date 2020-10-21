4f4dcc3df54b0346f9c5ddbbb38a6799698f2ce6
Jacob Duncan <jacobduncan00@yahoo.com>
2020-10-20 21:06:55 -0400

    will this fix the issue?

--- pages/reusables/large-card/active.tsx
+++ pages/reusables/large-card/active.tsx
@@ -1,26 +1,36 @@
 import React, { useState } from "react";
 import styles from "../../../styles/Home.module.css";
 
-type props = {
-  elements: { name: string }[];
+type Element = {
+  name: string;
 };
 
-const Active = ({ elements }: props) => {
-  const [active] = useState(elements);
+type Props = {
+  elements: Element[];
+};
+
+const Active = (props: Props) => {
+  let { elements } = props;
+  // const [active] = useState(elements);
 
   const [activeIndex, setActiveIndex] = useState(99); // figure out if I want to keep this 0 meaning it is the first button or if I want to make it out of reach "99" meaning that none of the buttons are activated
 
-  return active.map((u, i) => {
-    return (
-      <button
-        className={i === activeIndex ? styles.active : styles.settings_button}
-        onClick={() => setActiveIndex(i)}
-        key={u.name}
-      >
-        {u.name}
-      </button>
-    );
-  });
+  return (
+    <>
+      {elements &&
+        elements.map((u, i) => {
+          <button
+            className={
+              i === activeIndex ? styles.active : styles.settings_button
+            }
+            onClick={() => setActiveIndex(i)}
+            key={u.name}
+          >
+            {u.name}
+          </button>;
+        })}
+    </>
+  );
 };
 
 export default Active;
