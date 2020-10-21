fe813a384a680dfcfef356de72e3e95450b3343e
Jacob Duncan <jacobduncan00@yahoo.com>
2020-10-21 14:17:46 -0400

    fixed issue with svg not showing

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import LargeCard from "../../components/large-card";
 import styles from "../../styles/Home.module.css";
-import { ReactComponent as BackButton } from "./back-button.svg";
 
 const Settings = () => {
   return (
-    <div className="styles.setting_headercontainer">
-      {/* <img src="./back-button.svg" alt="Back Button SVG Image" /> */}
-      
+    <div className="styles.setting_headercontainer">      
       <h1 className={styles.settings_header}>Settings</h1>
       <LargeCard />
     </div>
