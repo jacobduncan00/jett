3504a956ba6fdf94f6d22630b70af896cf55642a
Jacob Duncan <jacobduncan00@yahoo.com>
2020-10-26 16:46:59 -0400

    got back button to work on settings page

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import LargeCard from "../../components/large-card";
 import styles from "../../styles/Home.module.css";
+import { FaChevronLeft } from "react-icons/fa";
+import Link from "next/link";
 
 const Settings = () => {
   return (
-    <div className="styles.setting_headercontainer">      
-      <h1 className={styles.settings_header}>Settings</h1>
+    <div className={styles.setting_headercontainer}>
+      <Link href="/">
+        <div className={styles.wrapperDiv}>
+          <a className={styles.chevronLink} href="/" >
+            <FaChevronLeft size={32} />
+          </a>
+          <h1 className={styles.settings_header}>Settings</h1>
+        </div>
+      </Link>
       <LargeCard />
-    </div>
+    </div >
   );
 };
 
