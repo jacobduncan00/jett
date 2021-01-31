ef153210715a25fad7da8c40ec620357daef5244
Jacob Duncan <jacob@pop-os.localdomain>
2021-01-31 13:58:54 -0500

    got the leaderboard rendering, but need the positions to be correct

--- pages/leaderboard/index.tsx
+++ pages/leaderboard/index.tsx
@@ -1,28 +1,52 @@
-import { useEffect, useState, useCallback } from "react";
-import axios, { AxiosError, AxiosResponse } from "axios";
-import { LeaderboardEntry } from "../../Types/LeaderBoardEntry";
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import {LeaderboardEntry} from '../../Types/LeaderBoardEntry';
+import LeaderboardCard from '../../components/leaderboard-card/index';
 
 const leaderboard = () => {
-    const [leaderboardEntries, setLeaderboardEntries] = useState<LeaderboardEntry[]>([]);
-    useEffect(() => {
-      async function fetchData() {
-        try {
-          const response = await axios.get("/api/leaderboard");
-          setLeaderboardEntries(response.data);
-        } catch(e) {
-          console.log(e);
-        }
-      };
-      fetchData();
-    }, []);
+  const [leaderboardEntries, setLeaderboardEntries] = useState<
+    LeaderboardEntry[]
+  >([]);
 
+  const [pos, setPosition] = useState(1);
 
-    return(
-      <div>{leaderboardEntries.map((entry) => (
-         <h1>{entry.username}</h1>
-      ))}</div>
-    )
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('/api/leaderboard');
+        response.data.forEach((entry: LeaderboardEntry) => {
+          entry.position = pos;
+          console.log(entry.position);
+          setPosition(pos + 1);
+        });
+        setLeaderboardEntries(response.data.sort(function(a: LeaderboardEntry, b: LeaderboardEntry) {
+          // b - a = descending
+          // a -b = ascending
+          return b.wpm - a.wpm;
+        }));
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    fetchData();
+  }, []);
 
-}
+  return (
+    <div>
+      <h1 className="leaderboard-header">Leaderboard</h1>
+      {leaderboardEntries.map(entry => (
+        <LeaderboardCard
+        key={Math.random().toString(36).substr(2, 9)} // random key to make error go away
+          className="color: red;"
+          position={entry.position}
+          username={entry.username}
+          wpm={entry.wpm}
+          errors={entry.errors}
+          accuracy={entry.accuracy}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default leaderboard;
