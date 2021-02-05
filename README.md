bbfdcceb286c4391160626b556f73af2b078e6c8
Jacob <jacobduncan00@yahoo.com>
2021-02-05 11:13:14 -0500

    adding some TODOs

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
-# Jett - An interactive typing game for programmers [WIP]
+# Jett - An interactive typing game [WIP]
 
-<p align="center">
-  <img src="https://static.wikia.nocookie.net/valorant/images/7/79/Jett_artwork.png/revision/latest?cb=20200602020209">
-</p>
+# TODO
+
+- [x] Decrement WPM if they user stops typing and the time runs out, if not it will stay at what wpm they had when they finished typing
+- [x] dockerize the frontend
+- [x] get leaderboard functionality working
+- [] create more percise queries from the database to get better statistics
+- [] implement d3js or reactchart to show some cool statistics about typers
+- [] /leaderboard redesign
+- [] remove settings & theme from /homepage until I finish it
+- [] dockerize a mysql server to startup when the frontend does
 
 # UPDATE
 
 - The postgres / heroku connection works on vercel!
-
-# TODO
-
-- Decrement WPM if they user stops typing and the time runs out, if not it will stay at what wpm they had when they finished typing
--- pages/leaderboard/index.tsx
+++ pages/leaderboard/index.tsx
@@ -1,7 +1,7 @@
-import {useEffect, useState} from 'react';
-import axios from 'axios';
-import {LeaderboardEntry} from '../../Types/LeaderBoardEntry';
-import LeaderboardCard from '../../components/leaderboard-card/index';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { LeaderboardEntry } from "../../types/LeaderBoardEntry";
+import LeaderboardCard from "../../components/leaderboard-card/index";
 
 const leaderboard = () => {
   const [leaderboardEntries, setLeaderboardEntries] = useState<
@@ -13,17 +13,22 @@ const leaderboard = () => {
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await axios.get('/api/leaderboard');
+        const response = await axios.get("/api/leaderboard");
         response.data.forEach((entry: LeaderboardEntry) => {
           entry.position = pos;
           console.log(entry.position);
           setPosition(pos + 1);
         });
-        setLeaderboardEntries(response.data.sort(function(a: LeaderboardEntry, b: LeaderboardEntry) {
-          // b - a = descending
-          // a -b = ascending
-          return b.wpm - a.wpm;
-        }));
+        setLeaderboardEntries(
+          response.data.sort(function (
+            a: LeaderboardEntry,
+            b: LeaderboardEntry
+          ) {
+            // b - a = descending
+            // a -b = ascending
+            return b.wpm - a.wpm;
+          })
+        );
       } catch (e) {
         console.log(e);
       }
@@ -34,9 +39,9 @@ const leaderboard = () => {
   return (
     <div>
       <h1 className="leaderboard-header">Leaderboard</h1>
-      {leaderboardEntries.map(entry => (
+      {leaderboardEntries.map((entry) => (
         <LeaderboardCard
-        key={Math.random().toString(36).substr(2, 9)} // random key to make error go away
+          key={Math.random().toString(36).substr(2, 9)} // random key to make error go away TODO: Better way of doing this
           className="color: red;"
           position={entry.position}
           username={entry.username}
